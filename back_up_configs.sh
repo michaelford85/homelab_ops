4ea0c8bfeb4bdcbe5eeeb224c56629ca1800af62
Michael Ford <michaelford85@gmail.com>
2026-02-07 13:49:17 -0600

    added jellyfin backup

--- back_up_configs.sh
+++ back_up_configs.sh
@@ -3,15 +3,16 @@
 #!/bin/bash
 
 # Step 1: Source into a particular Python virtual environment
-source ~/venvs/ansible/bin/activate
+source ~/.venvs/ansible/bin/activate
 
 # Step 2: Change into a directory
-cd ~/git-workspace/homelab_ops/
+# cd ~/git-workspace/homelab_ops/
+cd /workspace/git-workspace/homelab_ops/
 
 # Step 3: Run 2 Ansible playbooks from within the directory
 ansible-playbook -T 600 router-backup.yml -v
 ansible-playbook -T 600 pihole-backup.yml -v
 ansible-playbook -T 600 unifi-backup.yml -v
-
+ansible-playbook -T 600 jellyfin-backup.yml -v
 # Step 4: Deactivate the virtual environment at the end
 deactivate
